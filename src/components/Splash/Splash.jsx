f1b24c7858cf06b0406c2d89b62ee0156fef5db3
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-03-20 16:47:55 -0600

    more work on splash

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './splash.css';
+import '../../App.css'
 import logo from '../../images/logo.png';
 
 
@@ -11,7 +12,6 @@ export default function Splash() {
         <div className="splash-head">
             <img src = {logo} alt="logo" className="splash-image"/>
             <div className="splash-title">Sammie Bee Studios</div>
-            <img src = {logo} alt="logo" className="splash-image"/>
         </div>
         <div className="splash-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/mbD5ke7xqww?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
 
