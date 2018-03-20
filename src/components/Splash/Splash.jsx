98d4db050656f2e63d163ffdb57517958e8f96e2
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-03-20 16:20:52 -0600

    splash page with place holder video

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './splash.css';
+import logo from '../../images/logo.png';
 
 
 export default function Splash() {
     return(
-        <div>
+        <div className = "splash-main">
+
+            <div className ="splash-container">
+        <div className="splash-head">
+            <img src = {logo} alt="logo" className="splash-image"/>
+            <div className="splash-title">Sammie Bee Studios</div>
+            <img src = {logo} alt="logo" className="splash-image"/>
+        </div>
+        <div className="splash-video"><iframe width="560" height="315" src="https://www.youtube.com/embed/mbD5ke7xqww?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+
+                <button className ="splash-button">Welcome!</button>
+            </div>
 
         </div>
     )
